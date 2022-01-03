07069f3f7f33d1bc236891e60bd6edc82fe2aa5f
Wojtek <czerwinskiwtj@gmail.com>
2022-01-04 00:06:30 +0100

    GameManager

--- test/Assets/Scripts/GameManager.cs
+++ test/Assets/Scripts/GameManager.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private void Update()
-    {
-        
-    }
+    public static GameManager instance; // Pozwala to odnosic sie do tego skryptu w innych skryptach bez tworzenia obiektu GameManager - wystarczy GameManager.instance.gold (np.) ;
+                                        // Bedzie stad mozna zarzadzac scenami (teleportacja gracza) 
+
+    public int points;
+
 }
